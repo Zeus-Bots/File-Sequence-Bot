6ff8aac464ec78c8db5087266642a91a324f8362
Zeus-Bots <zeusbots9@gmail.com>
2025-12-25 20:25:19 +0600

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ START_PIC = "https://telegra.ph/file/c39602ab8e3d5932a91d1.jpg"
 START_MSG = "<b>Hi There...! \n\nI am a File Sequence Bot.\nI can sequence your files effortlessly.\n\nMy Owner: @Its_Sahil_Ansari</b>"
 HELP_TXT = "<b><i>About Us..\n\n‣ Made for : Anime x Hunters\n‣ Owner : @Its_Sahil_Ansari\n‣ Worked For : @Anime_x_hunters</i></b>"
 OWNER_ID = 5090651635
+Log_Channel =
